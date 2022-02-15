e43a48719d2a49edb068e8df5d0a7bc704c9d1b5
Parsa Nasirimehr <parsa.nasirimehr@dietdoctor.com>
2022-02-15 12:18:44 +0330

    fixed JNI return types

--- android/cpp-adapter.cpp
+++ android/cpp-adapter.cpp
@@ -2,13 +2,13 @@
 #include "react-native-hashing.h"
 
 extern "C"
-JNIEXPORT jint JNICALL
+JNIEXPORT void JNICALL
 Java_com_reactnativehashing_HashingModule_initialize(JNIEnv *env, jclass clazz, jlong jsiPtr) {
     RNHashing::installHashing(*reinterpret_cast<facebook::jsi::Runtime *>(jsiPtr));
 }
 
 extern "C"
-JNIEXPORT jint JNICALL
+JNIEXPORT void JNICALL
 Java_com_reactnativehashing_HashingModule_destruct(JNIEnv *env, jclass clazz) {
     RNHashing::cleanUpHashing();
 }
