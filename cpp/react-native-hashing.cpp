0866f0ef1d481a7825867a8c8944267fcfffe7bb
Parsa Nasirimehr <p.nasirimehr@gmail.com>
2022-03-25 23:13:16 +0430

    remove generic type and reimplemented the sha1 and sha3 manually

--- cpp/react-native-hashing.cpp
+++ cpp/react-native-hashing.cpp
@@ -4,69 +4,76 @@ using namespace facebook;
 using namespace std;
 
 namespace RNHashing {
-    string hashValue(const string &input, const HashTypes type) {
-        string result = {};
-        if (type == sha256) {
-            result = sha256Convertor(input);
-        }
-        
-        if (type == md5) {
-            result = md5Convertor(input);
-        }
-        
-        if (type == sha1) {
-            result = sha1Convertor(input);
-        }
-        
-        if (type == sha3) {
-            result = sha3Convertor(input);
-        }
-        return result;
-    }
-
-    string nameFromType(const HashTypes type) {
-        string result = {};
-        if (type == sha256) {
-            result = "sha256";
-        }
-        
-        if (type == md5) {
-            result = "md5";
-        }
-        
-        if (type == sha1) {
-            result = "sha1";
-        }
-        
-        if (type == sha3) {
-            result = "sha3";
-        }
-        return result;
-    }
-    
     void installHashing(jsi::Runtime &rt) {
-        jsi::Object module = jsi::Object(rt);
-        for (int i = sha256; i <= sha3; i++) {
-            auto hashFunction = jsi::Function::createFromHostFunction(rt,
-                                                                      jsi::PropNameID::forAscii(rt, nameFromType(HashTypes(i))),
-                                                                      1,
-                                                                      [&i](jsi::Runtime &rt, const jsi::Value &thisValue, const jsi::Value *args, size_t count) -> jsi::Value {
-                string argCountError = nameFromType(HashTypes(i)) + " requires one arguement";
-                string argTypeError = "arguement for " + nameFromType(HashTypes(i)) + " needs to be of type string";
-                if (count == 0 || count > 1) {
-                      jsi::detail::throwJSError(rt, argCountError.c_str());
-                      return jsi::Value::undefined();
-                }
-                if (!args[0].isString()) {
-                     jsi::detail::throwJSError(rt, argTypeError.c_str());
-                     return jsi::Value::undefined();
-                }
-                string input = args[0].asString(rt).utf8(rt);
-                return jsi::Value(rt, jsi::String::createFromUtf8(rt, hashValue(input, HashTypes(i))));
-                }
-            );
-            module.setProperty(rt, nameFromType(HashTypes(i)).c_str(), move(hashFunction));
+        auto sha256 = jsi::Function::createFromHostFunction(rt,
+                                                            jsi::PropNameID::forAscii(rt, "sha256"),
+                                                            1,
+                                                            [](jsi::Runtime &rt, const jsi::Value &thisValue, const jsi::Value *args, size_t count) -> jsi::Value {
+            if (count == 0 || count > 1) {
+                jsi::detail::throwJSError(rt, "sha256 requires one arguement");
+                return jsi::Value::undefined();
+            }
+            if (!args[0].isString()) {
+                jsi::detail::throwJSError(rt, "arguement for sha256 needs to be of type string");
+                return jsi::Value::undefined();
+            }
+            string input = args[0].asString(rt).utf8(rt);
+            return jsi::Value(rt, jsi::String::createFromUtf8(rt, sha256Convertor(input)));
         }
+                                );
+        auto md5 = jsi::Function::createFromHostFunction(rt,
+                                                            jsi::PropNameID::forAscii(rt, "md5"),
+                                                            1,
+                                                            [](jsi::Runtime &rt, const jsi::Value &thisValue, const jsi::Value *args, size_t count) -> jsi::Value {
+            if (count == 0 || count > 1) {
+                jsi::detail::throwJSError(rt, "md5 requires one arguement");
+                return jsi::Value::undefined();
+            }
+            if (!args[0].isString()) {
+                jsi::detail::throwJSError(rt, "arguement for md5 needs to be of type string");
+                return jsi::Value::undefined();
+            }
+            string input = args[0].asString(rt).utf8(rt);
+            return jsi::Value(rt, jsi::String::createFromUtf8(rt, md5Convertor(input)));
+        }
+                                                                 );
+        auto sha1 = jsi::Function::createFromHostFunction(rt,
+                                                            jsi::PropNameID::forAscii(rt, "sha1"),
+                                                            1,
+                                                            [](jsi::Runtime &rt, const jsi::Value &thisValue, const jsi::Value *args, size_t count) -> jsi::Value {
+            if (count == 0 || count > 1) {
+                jsi::detail::throwJSError(rt, "sha1 requires one arguement");
+                return jsi::Value::undefined();
+            }
+            if (!args[0].isString()) {
+                jsi::detail::throwJSError(rt, "arguement for sha1 needs to be of type string");
+                return jsi::Value::undefined();
+            }
+            string input = args[0].asString(rt).utf8(rt);
+            return jsi::Value(rt, jsi::String::createFromUtf8(rt, sha1Convertor(input)));
+        }
+                                                                 );
+        auto sha3 = jsi::Function::createFromHostFunction(rt,
+                                                            jsi::PropNameID::forAscii(rt, "sha3"),
+                                                            1,
+                                                            [](jsi::Runtime &rt, const jsi::Value &thisValue, const jsi::Value *args, size_t count) -> jsi::Value {
+            if (count == 0 || count > 1) {
+                jsi::detail::throwJSError(rt, "sha3 requires one arguement");
+                return jsi::Value::undefined();
+            }
+            if (!args[0].isString()) {
+                jsi::detail::throwJSError(rt, "arguement for sha3 needs to be of type string");
+                return jsi::Value::undefined();
+            }
+            string input = args[0].asString(rt).utf8(rt);
+            return jsi::Value(rt, jsi::String::createFromUtf8(rt, sha3Convertor(input)));
+        }
+                                                                 );
+        jsi::Object module = jsi::Object(rt);
+        module.setProperty(rt, "sha256", move(sha256));
+        module.setProperty(rt, "md5", move(md5));
+        module.setProperty(rt, "sha1", move(sha1));
+        module.setProperty(rt, "sha3", move(sha3));
         rt.global().setProperty(rt, "RNHashing", move(module));
     }
     void cleanUpHashing() {
--- cpp/react-native-hashing.h
+++ cpp/react-native-hashing.h
@@ -7,14 +7,10 @@
 #include "hash-library/md5.h"
 #include "hash-library/sha1.h"
 #include "hash-library/sha3.h"
-#include <map>
-
 
 using namespace std;
 
 namespace RNHashing {
-    enum HashTypes { sha256, md5, sha1, sha3 };
-    static HashTypes hTypes;
     static SHA256 sha256Convertor;
     static MD5 md5Convertor;
     static SHA1 sha1Convertor;
